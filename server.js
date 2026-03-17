a144ff209695ddbeb5216f31dbf8440c5cfa46f3
CodeMaster2099 <id2497510@gmail.com>
2026-03-17 17:13:01 +0000

    serve html files

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-﻿const { createServer } = require('http');
+const { createServer } = require('http');
 const { WebSocketServer } = require('ws');
+const fs = require('fs');
+const path = require('path');
 
 const server = createServer((req, res) => {
-  res.writeHead(200);
-  res.end('phone-relay alive');
+  if (req.url === '/phone-hub') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream(path.join(__dirname, 'phone-hub.html')).pipe(res);
+  } else if (req.url === '/dashboard') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream(path.join(__dirname, 'dashboard.html')).pipe(res);
+  } else {
+    res.writeHead(200);
+    res.end('phone-relay alive');
+  }
 });
 
 const wss = new WebSocketServer({ server });
