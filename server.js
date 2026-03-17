5ed972c7b73edaaab081ba20ab427f47102d85fe
CodeMaster2099 <id2497510@gmail.com>
2026-03-17 15:44:41 +0000

    initial relay

---
+++ server.js
@@ -0,0 +1,27 @@
+﻿const { createServer } = require('http');
+const { WebSocketServer } = require('ws');
+
+const server = createServer((req, res) => {
+  res.writeHead(200);
+  res.end('phone-relay alive');
+});
+
+const wss = new WebSocketServer({ server });
+const clients = new Set();
+
+wss.on('connection', ws => {
+  clients.add(ws);
+  console.log('client connected, total:', clients.size);
+  ws.on('message', msg => {
+    clients.forEach(c => {
+      if (c !== ws && c.readyState === 1) c.send(msg);
+    });
+  });
+  ws.on('close', () => {
+    clients.delete(ws);
+    console.log('client left, total:', clients.size);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log('relay running on port', PORT));
